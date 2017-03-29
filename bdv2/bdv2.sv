b7d0c3f0fe4708711567124a32e138131d6f42ac
Rafael Pontes <rafaelgpontes1@gmail.com>
2017-03-29 17:45:04 -0300

    top level com rx_enable

--- bdv2/bdv2.sv
+++ bdv2/bdv2.sv
@@ -1,25 +1,28 @@
 module bdv2 ();
   input logic [7:0]ADC,
+  input logic [7:0]DATA_IN,
+  input logic [7:0]ADDRESS,
+  output logic [7:0]DATA_OUT,
   input logic G_CLK_RX,
   input logic reset,
+  input logic write_enabe,
   output logic int_rx_host
-);
+);	  
+	logic [15:0]data_dm;
+	logic status;
+	logic RX_ENABLE;
+	
+	always_comb begin
+		if(G_CLK_RX && RX_ENABLE) begin
+			Demodulador DM(.G_CLK_RX(G_CLK_RX),
+								.in_signal(ADC),
+								.reset(reset),
+								.status(status),
+								.data(data_dm));
+			Decodificador DC();
+		end
+	end
+	
+	RegisterField RF();
   
-
-logic [7:0] data_out_dem_0;
-logic [7:0] data_out_dem_1;
-logic [7:0] address_RF;
-logic [7:0] BD_DATA_0;
-logic [7:0] BD_DATA_1;
-logic [7:0] BD_DATA_0_DECOD;
-logic [7:0] BD_DATA_1_DECOD;
-  
-  Demodulador M1();
-  
-  Decodificador M2();
-  
-  RegisterField M3();
-  
-  AMBA amba (.valid(), .data_in(), .address(), .data_out(), .ready());
-
 endmodule 
