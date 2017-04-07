29b8d485266b879de4c9c20d556ef1928c1eb38e
maires1306 <maires1306@gmail.com>
2017-04-07 17:40:25 -0300

    Update Decodificador.sv

--- bdv2/Decodificador.sv
+++ bdv2/Decodificador.sv
@@ -4,7 +4,15 @@ module Decodificador(
 );
 
 always_comb begin
-  BD_DATA_0[7] <= data_0[7];
+  BD_DATA_0[15] <= data_0[15];
+  BD_DATA_0[14] <= BD_DATA_0[15] ^ data_0[14];
+  BD_DATA_0[13] <= BD_DATA_0[14] ^ data_0[13];
+  BD_DATA_0[12] <= BD_DATA_0[13] ^ data_0[12];
+  BD_DATA_0[11] <= BD_DATA_0[12] ^ data_0[11];
+  BD_DATA_0[10] <= BD_DATA_0[11] ^ data_0[10];
+  BD_DATA_0[9] <= BD_DATA_0[10] ^ data_0[9];
+  BD_DATA_0[8] <= BD_DATA_0[9] ^ data_0[8];
+  BD_DATA_0[7] <= BD_DATA_0[8] ^ data_0[7];
   BD_DATA_0[6] <= BD_DATA_0[7] ^ data_0[6];
   BD_DATA_0[5] <= BD_DATA_0[6] ^ data_0[5];
   BD_DATA_0[4] <= BD_DATA_0[5] ^ data_0[4];
