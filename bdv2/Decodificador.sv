bddefe816d07873cb89d4bbd22417207a21fa8c3
maires1306 <maires1306@gmail.com>
2017-04-09 16:41:30 -0300

    Update Decodificador.sv

--- bdv2/Decodificador.sv
+++ bdv2/Decodificador.sv
@@ -4,22 +4,22 @@ module Decodificador(
 );
 
 always_comb begin
-  BD_DATA_0[15] <= data_0[15];
-  BD_DATA_0[14] <= BD_DATA_0[15] ^ data_0[14];
-  BD_DATA_0[13] <= BD_DATA_0[14] ^ data_0[13];
-  BD_DATA_0[12] <= BD_DATA_0[13] ^ data_0[12];
-  BD_DATA_0[11] <= BD_DATA_0[12] ^ data_0[11];
-  BD_DATA_0[10] <= BD_DATA_0[11] ^ data_0[10];
-  BD_DATA_0[9] <= BD_DATA_0[10] ^ data_0[9];
-  BD_DATA_0[8] <= BD_DATA_0[9] ^ data_0[8];
-  BD_DATA_0[7] <= BD_DATA_0[8] ^ data_0[7];
-  BD_DATA_0[6] <= BD_DATA_0[7] ^ data_0[6];
-  BD_DATA_0[5] <= BD_DATA_0[6] ^ data_0[5];
-  BD_DATA_0[4] <= BD_DATA_0[5] ^ data_0[4];
-  BD_DATA_0[3] <= BD_DATA_0[4] ^ data_0[3];
-  BD_DATA_0[2] <= BD_DATA_0[3] ^ data_0[2];
-  BD_DATA_0[1] <= BD_DATA_0[2] ^ data_0[1];
-  BD_DATA_0[0] <= BD_DATA_0[1] ^ data_0[0];
+  BD_DATA[15] <= data[15];
+  BD_DATA[14] <= BD_DATA[15] ^ data[14];
+  BD_DATA[13] <= BD_DATA[14] ^ data[13];
+  BD_DATA[12] <= BD_DATA[13] ^ data[12];
+  BD_DATA[11] <= BD_DATA[12] ^ data[11];
+  BD_DATA[10] <= BD_DATA[11] ^ data[10];
+  BD_DATA[9] <= BD_DATA[10] ^ data[9];
+  BD_DATA[8] <= BD_DATA[9] ^ data[8];
+  BD_DATA[7] <= BD_DATA[8] ^ data[7];
+  BD_DATA[6] <= BD_DATA[7] ^ data[6];
+  BD_DATA[5] <= BD_DATA[6] ^ data[5];
+  BD_DATA[4] <= BD_DATA[5] ^ data[4];
+  BD_DATA[3] <= BD_DATA[4] ^ data[3];
+  BD_DATA[2] <= BD_DATA[3] ^ data[2];
+  BD_DATA[1] <= BD_DATA[2] ^ data[1];
+  BD_DATA[0] <= BD_DATA[1] ^ data[0];
 end
 
 endmodule
